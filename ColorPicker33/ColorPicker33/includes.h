80cf0d45c4709433a9f743945a0304ebbd1ddd0f
HarelGilad <harelgilad303@gmail.com>
2025-03-09 17:54:34 +0200

    Added main & includes files

---
+++ ColorPicker33/ColorPicker33/includes.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDES
+#define INCLUDES
+
+#include <windows.h>
+
+#endif
---
+++ ColorPicker33/ColorPicker33/main.cpp
@@ -0,0 +1,6 @@
+#include "includes.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+    return 0;
+}
